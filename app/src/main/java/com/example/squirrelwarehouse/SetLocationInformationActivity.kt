9d8e72aae78b1939b715aa97bb285812f0d05a4c
sewon <sewon@gmail.com>
2021-02-27 15:22:42 +0900

    위치정보설정 페이지, 폰트 정의, 스위치 정의 등

---
+++ app/src/main/java/com/example/squirrelwarehouse/SetLocationInformationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.squirrelwarehouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SetLocationInformationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_location_information)
+    }
+}
